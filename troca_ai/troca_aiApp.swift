f8c46d032ff3945bc6547fc9f82b863c39dcb6f1
luan <luanbispo.sousa263@gmail.com>
2025-05-13 20:45:39 -0300

    Initial Commit

---
+++ troca_ai/troca_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  troca_aiApp.swift
+//  troca_ai
+//
+//  Created by luan on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct troca_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ troca_aiTests/troca_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  troca_aiTests.swift
+//  troca_aiTests
+//
+//  Created by luan on 13/05/25.
+//
+
+import Testing
+@testable import troca_ai
+
+struct troca_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
